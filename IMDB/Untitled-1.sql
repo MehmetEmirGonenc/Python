404d68fa2b0efd3adbea0993bc89822282b046da
Mehmet Gönenç <mehmetemirgonenc@gmail.com>
2023-08-05 15:36:23 +0300

    List_mov

---
+++ IMDB/Untitled-1.sql
@@ -0,0 +1,8 @@
+
+SELECT primary_title, name,premiered FROM titles INNER JOIN crew ON  titles.title_id = crew.title_id INNER JOIN people ON crew.person_id = people.person_id 
+WHERE primary_title = "Oppenheimer" AND crew.category = 'director' ORDER BY premiered DESC;
+
+SELECT primary_title FROM titles INNER JOIN ratings ON titles.title_id = ratings.title_id WHERE titles.title_id IN (SELECT title_id FROM crew WHERE category = 'director' AND person_id = (SELECT person_id FROM people WHERE name = 'Woody Allen' AND born = 1935)) ORDER BY ratings.rating DESC;
+
+
+SELECT * FROM (SELECT ROW_NUMBER() OVER(ORDER BY ratings.rating * ratings.votes DESC) as id, titles.title_id,primary_title,premiered,ended,type,runtime_minutes,genres,ratings.rating FROM titles INNER JOIN ratings ON titles.title_id = ratings.title_id) WHERE id BETWEEN 20 AND 30;
