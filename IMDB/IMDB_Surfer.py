08cc1d11f8ad3b56c4aca4ccc74f5d1ef08101ee
Mehmet Gönenç <mehmetemirgonenc@gmail.com>
2023-08-06 12:55:50 +0300

    V0.1

--- IMDB/IMDB_Surfer.py
+++ IMDB/IMDB_Surfer.py
@@ -3,8 +3,6 @@ import sqlite3
 def main():
     con = sqlite3.connect("imdb.db")
     cur = con.cursor()
-
-    
     #Main Loop
     while (True):
         #Main menu
@@ -26,27 +24,55 @@ def main():
 
 # List movies function
 def List_Mov(cur):
+    List(cur, "SELECT * FROM (SELECT ROW_NUMBER() OVER(ORDER BY ratings.rating * ratings.votes DESC) as id, titles.title_id,primary_title,premiered,ended,type,runtime_minutes,genres,ratings.rating,ratings.votes FROM titles INNER JOIN ratings ON titles.title_id = ratings.title_id) WHERE id BETWEEN ? AND ?;")
+
+        
+#Browsing
+def Browse(cur):
+    List(cur,"SELECT * FROM (SELECT ROW_NUMBER() OVER(ORDER BY ratings.rating * ratings.votes DESC) as id, titles.title_id,primary_title,premiered,ended,type,runtime_minutes,genres,ratings.rating,ratings.votes FROM titles INNER JOIN ratings ON titles.title_id = ratings.title_id WHERE primary_title LIKE ?) WHERE id BETWEEN ? AND ?;", 'Browse')
+
+#Call crew details
+def Call_Crew(cur, title_id):
+    #Get crew information with sql query
+    detail_res = cur.execute("SELECT category, people.name FROM crew INNER JOIN people ON crew.person_id = people.person_id WHERE crew.title_id = ? ORDER BY CASE WHEN category = 'director' THEN 1 WHEN category = 'writer' THEN 2 WHEN category = 'actor' THEN 3 ELSE 4 END ASC;", title_id)
+    return detail_res.fetchall()
+
+
+#Listings
+def List(cur, query, mode = ' '):
     page = 0
+    brow_key = False
     #Main Loop
     while (True):
         page += 1
-        place_holder = [(((page - 1)*100) + 1), (page*100)]
-        res = cur.execute("SELECT * FROM (SELECT ROW_NUMBER() OVER(ORDER BY ratings.rating * ratings.votes DESC) as id, titles.title_id,primary_title,premiered,ended,type,runtime_minutes,genres,ratings.rating,ratings.votes FROM titles INNER JOIN ratings ON titles.title_id = ratings.title_id) WHERE id BETWEEN ? AND ?;", place_holder)
-        values = res.fetchall()
-
+        if mode == 'Browse':
+            if brow_key == False:
+                print("Plese enter name of movie")
+                movie_name = input(">>>")
+            args = [('%' + movie_name + '%'), (((page - 1)*50) + 1), (page*50)]
+            res = cur.execute(query, args)
+            values = res.fetchall()
+        else:
+            args = [(((page - 1)*50) + 1), (page*50)]
+            res = cur.execute(query, args)
+            values = res.fetchall()
         for row in values:
             print(f"+----------------------------- ( {row[0]} ) -------------------------------------------+")
-            print(f"| Name : {row[2]}          {row[3]}/{row[4]}")
+            print(f"| Name : {row[2]}          {row[3]}/{row[4]}")          
             print(f"| Type : {row[5]}          {row[6]}min        Genres : {row[7]}")
+            print(f"| IMDB : {row[8]} ")
             print("+---------------------------------------------------------------------------------------+")
         print(f"\n                                   <{page}>")    
         while (True):
             print("\nN - > Next Page")
             print("P - > Previous Page")
             print("<movie id> - > Detailed information about movie")
+            if mode == 'Browse':
+                print("B - > Browse something else")
             print("E - > Exit")
             choice = input(">>>")
             if choice.upper() == "N" :
+                brow_key = True
                 break
             elif choice.upper() == "E":
                 return
@@ -56,6 +82,10 @@ def List_Mov(cur):
                 else:
                     page -= 2
                 break 
+            elif choice.upper() == "B" and mode == 'Browse':
+                page = 0
+                brow_key = False
+                break
             else:
                 if choice.isalpha() == False:
                     for row in values:
@@ -72,33 +102,4 @@ def List_Mov(cur):
                             print(f"*********************************************************")
                 else:
                     print("Unexpected input!")
-
-
-#Browsing
-def Browse(cur):
-    page = 0
-    #Main loop
-    while(True):
-        page += 1
-        print("########################## Movie Browser ##########################")
-        print("Plese enter name of movie")
-        movie_name = input(">>>")
-        args = [movie_name, ((page-1) * 50), (page * 50)]
-        browse = cur.execute("SELECT id, title_id, type, primary_title, premiered, ended, runtime_minutes, genres, rating, votes FROM (SELECT ROW_NUMBER() OVER(ORDER BY ratings.rating * ratings.votes DESC) as id, * FROM titles INNER JOIN ratings ON titles.title_id = ratings.title_id WHERE primary_title LIKE '%?%') WHERE id BETWEEN ? AND ?;",args)
-        resaults = browse.fetchall()
-        for row in resaults:
-            print(f"+----------------------------- ( {row[0]} ) -------------------------------------------+")
-            print(f"| Name : {row[3]}          {row[4]}/{row[5]}")
-            print(f"| Type : {row[2]}          {row[6]}min        Genres : {row[7]}")
-            print("+---------------------------------------------------------------------------------------+")
-        print(f"\n                                   <{page}>")    
-
-
-#Call crew details
-def Call_Crew(cur, title_id):
-    #Get crew information with sql query
-    detail_res = cur.execute("SELECT category, people.name FROM crew INNER JOIN people ON crew.person_id = people.person_id WHERE crew.title_id = ? ORDER BY CASE WHEN category = 'director' THEN 1 WHEN category = 'writer' THEN 2 WHEN category = 'actor' THEN 3 ELSE 4 END ASC;", title_id)
-    return detail_res.fetchall()
-
-
 main()
